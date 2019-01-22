694bd76e8521f2110dfbe532a9437563477f1a53
hefeng6500 <2443992009@qq.com>
2019-01-22 22:27:17 +0800

    练习了以下React-router，React.createClass 该api在v16版本已经废弃

--- src/index.js
+++ src/index.js
@@ -1,12 +1,73 @@
+
+// 做为React-router的练习
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+
+class About extends React.Component{
+  render(){
+    return (
+      <h1>About</h1>
+    )
+  }
+}
+class Inbox extends React.Component{
+  render(){
+    return (
+      <h1>Inbox</h1>
+    )
+  }
+}
+class Home extends React.Component{
+  render(){
+    return (
+      <h1>Home</h1>
+    )
+  }
+}
+
+class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      route: window.location.hash.substr(1)
+    }
+  }
+
+  componentDidMount() {
+    window.addEventListener('hashchange', () => {
+      this.setState({
+        route: window.location.hash.substr(1)
+      })
+    })
+  }
+
+  render() {
+    let Child
+    switch (this.state.route) {
+      case '/about': 
+        Child = About; 
+        break;
+      case '/inbox': 
+        Child = Inbox; 
+        break;
+      default: Child = Home;
+    }
+
+    return (
+      <div>
+        <h1>App</h1>
+        <ul>
+          <li><a href="#/about">About</a></li>
+          <li><a href="#/inbox">Inbox</a></li>
+        </ul>
+        <Child />
+      </div>
+    )
+  }
+
+}
+
+
+
 
 ReactDOM.render(<App />, document.getElementById('root'));
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
