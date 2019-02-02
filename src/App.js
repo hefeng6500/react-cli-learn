e685941a1c631da9475c303bd8465578d7584487
hefeng6500 <2443992009@qq.com>
2019-02-02 17:34:04 +0800

    添加了TodoList功能示例

--- src/App.js
+++ src/App.js
@@ -1,26 +1,94 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
+import { Input, Button, Icon } from 'antd';
 import './App.css';
+import './css/todolist.css'
+
+
+// class App extends Component {
+//   render() {
+//     return (
+//       <div className="App">
+//         <header className="App-header">
+//           <img src={logo} className="App-logo" alt="logo" />
+//           <p>
+//             Edit <code>src/App.js</code> and save to reload.
+//           </p>
+//           <a
+//             className="App-link"
+//             href="https://reactjs.org"
+//             target="_blank"
+//             rel="noopener noreferrer"
+//           >
+//             Learn React
+//           </a>
+//         </header>
+//       </div>
+//     );
+//   }
+// }
 
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      items: [
+        { id: "1", text: "first one" },
+        { id: "2", text: "first two" }
+      ],
+      inputText: ""
+    }
+  }
+
+  handleChange = (e) => {
+    this.setState({ inputText: e.target.value })
+  }
+
+  handSubmit = (e) => {
+    e.preventDefault();
+    if (!this.state.inputText.length) {
+      return
+    }
+    const item = {
+      id: Date.now(),
+      text: this.state.inputText
+    }
+
+    this.setState(state => ({
+      items: state.items.concat(item),
+      inputText: ""
+    }))
+  }
+
+  delete = (id) => {
+    let copy = this.state.items.concat()
+    this.setState({
+      items: copy.filter(item => item.id !== id)
+    })
+  }
+
+  render() {
+    const inputText = this.state.inputText
+    return (
+      <div>
+        <h1>TODO LIST</h1>
+        <Input onChange={this.handleChange} value={inputText} className="myinputBox" placeholder="Basic usage" />
+
+        <Button onClick={this.handSubmit} type="primary">Primary</Button>
+        <TodoList items={this.state.items} delete={id => this.delete(id)} />
+      </div>
+    )
+  }
+}
+
+class TodoList extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+      <ul>
+        {this.props.items.map(item => (
+          <li key={item.id}>{item.text} <span><Icon type="close" onClick={() => { this.props.delete(item.id) }} /></span></li>
+        ))}
+      </ul>
     );
   }
 }
--- src/index.js
+++ src/index.js
@@ -1,72 +1,8 @@
-import React, { Component } from "react"
+import React from "react"
 import ReactDOM from "react-dom"
-import { Divider } from "antd";
+import 'antd/dist/antd.css';
 
-// 通过获取hash值，修改Child内容，达到路由切换的效果
-class About extends Component {
-  render() {
-    return (
-      <h2>I am About</h2>
-    )
-  }
-}
+import App from "./App.js"
 
-class Inbox extends React.Component {
-  render() {
-    return (
-      <h2>I am Inbox</h2>
-    )
-  }
-}
-
-class Home extends React.Component {
-  render() {
-    return (
-      <h2>I am Home</h2>
-    )
-  }
-}
-
-class App extends React.Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      route: window.location.hash.substr(1)
-    }
-  }
-
-  componentDidMount() {
-    window.addEventListener("hashchange", () => {
-      this.setState({
-        route: window.location.hash.substr(1)
-      })
-    })
-  }
-
-  render() {
-    let Child
-    switch (this.state.route) {
-      case '/about':
-        Child = About
-        break;
-      case '/inbox':
-        Child = Inbox
-        break;
-      default:
-        Child = Home
-        break;
-    }
-    return (
-      <div>
-        <Home />
-        <ul>
-          <li><a href="#/about">about</a></li>
-          <li><a href="#/inbox">inbox</a></li>
-        </ul>
-        <Child />
-      </div>
-    )
-  }
-}
 
 ReactDOM.render(<App />, document.getElementById("root"))
