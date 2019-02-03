9f785c52847d5d2e891835805c9986b4fac270be
hefeng6500 <2443992009@qq.com>
2019-02-03 10:57:11 +0800

    重写todolist

--- src/App.js
+++ src/App.js
@@ -2,94 +2,77 @@ import React, { Component } from 'react';
 // import logo from './logo.svg';
 import { Input, Button, Icon } from 'antd';
 import './App.css';
-import './css/todolist.css'
 
-
-// class App extends Component {
-//   render() {
-//     return (
-//       <div className="App">
-//         <header className="App-header">
-//           <img src={logo} className="App-logo" alt="logo" />
-//           <p>
-//             Edit <code>src/App.js</code> and save to reload.
-//           </p>
-//           <a
-//             className="App-link"
-//             href="https://reactjs.org"
-//             target="_blank"
-//             rel="noopener noreferrer"
-//           >
-//             Learn React
-//           </a>
-//         </header>
-//       </div>
-//     );
-//   }
-// }
 
 class App extends Component {
   constructor(props) {
     super(props)
     this.state = {
       items: [
-        { id: "1", text: "first one" },
-        { id: "2", text: "first two" }
+        { id: '1', text: 'first one' },
+        { id: '2', text: 'second one' }
       ],
-      inputText: ""
+      inputValue: ""
     }
+
+    this.handleChange = this.handleChange.bind(this)
+    this.handleAdd = this.handleAdd.bind(this)
   }
 
   handleChange = (e) => {
-    this.setState({ inputText: e.target.value })
+    this.setState({ inputValue: e.target.value })
   }
 
-  handSubmit = (e) => {
-    e.preventDefault();
-    if (!this.state.inputText.length) {
+  handleAdd = () => {
+    if(!this.state.inputValue){
       return
     }
-    const item = {
+    let newItem = {
       id: Date.now(),
-      text: this.state.inputText
+      text: this.state.inputValue
     }
-
     this.setState(state => ({
-      items: state.items.concat(item),
-      inputText: ""
+      items: state.items.concat(newItem),
+      inputValue: ""
     }))
   }
 
-  delete = (id) => {
-    let copy = this.state.items.concat()
+  handleDelete = (id) => {
+    // let newItems = this.state.items.concat()
+    // newItems.filter(item=>item.id!==id)
     this.setState({
-      items: copy.filter(item => item.id !== id)
+      items: this.state.items.filter(item => item.id !== id)
     })
   }
 
   render() {
-    const inputText = this.state.inputText
     return (
-      <div>
-        <h1>TODO LIST</h1>
-        <Input onChange={this.handleChange} value={inputText} className="myinputBox" placeholder="Basic usage" />
-
-        <Button onClick={this.handSubmit} type="primary">Primary</Button>
-        <TodoList items={this.state.items} delete={id => this.delete(id)} />
+      <div className="container">
+        <h1>Todolist</h1>
+        <Input placeholder="Basic usage" className="myInput" value={this.state.inputValue} onChange={this.handleChange} />
+        <Button type="primary" className="btn" onClick={this.handleAdd}>add Data</Button>
+        <Todolist items={this.state.items} handleDelete={id => this.handleDelete(id)} />
       </div>
     )
   }
 }
 
-class TodoList extends Component {
+
+class Todolist extends Component {
   render() {
     return (
-      <ul>
-        {this.props.items.map(item => (
-          <li key={item.id}>{item.text} <span><Icon type="close" onClick={() => { this.props.delete(item.id) }} /></span></li>
-        ))}
-      </ul>
-    );
+      <div className="todolist">
+        <ul>
+          {
+            this.props.items.map(item => {
+              return (
+                <li key={item.id}>{item.text} <span><Icon type="close" onClick={() => this.props.handleDelete(item.id)} /></span></li>
+              )
+            })
+          }
+        </ul>
+      </div>
+    )
   }
 }
 
